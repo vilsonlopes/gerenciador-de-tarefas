fd727e7fd60511c45c3f36f3385ad03c3ccf2865
Vilson Lopes <vilsonlopes@yahoo.com.br>
2024-03-29 01:09:24 -0300

    add alteração

--- taskmanager/tasks/views.py
+++ taskmanager/tasks/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, ListView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+
+from .models import Task
+
 
 # Create your views here.
+class TaskListView(ListView):
+    model = Task
+    template_name = "task_list.html"
+    context_object_name = "tasks"
+
+
+class TaskDetailView(DetailView):
+    model = Task
+    template_name = "task_detail.html"
+    context_object_name = "task"
+
+
+class TaskCreateView(CreateView):
+    model = Task
+    template_name = "task_form.html"
+    fields = ("name", "description", "start_date", "end_date")
+
+    def get_success_url(self):
+        return reverse_lazy("task_detail", kwargs={"pk": self.object.id})
+
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    template_name = "task_form.html"
+    fields = ("name", "description", "start_date", "end_date")
+
+    def get_success_url(self):
+        return reverse_lazy("task_detail", kwargs={"pk": self.object.id})
+
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    template_name = "task_confirm_delete.html"
+    success_url = reverse_lazy("task-list")
